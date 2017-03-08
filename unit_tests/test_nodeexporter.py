1d4ebceffb51c9bac29ae390aa1b1655aa927f57
Bjorn Tillenius <bjorn@tillenius.me>
2017-03-08 13:39:12 +0000

    Cleanup the test setup.

--- unit_tests/test_nodeexporter.py
+++ unit_tests/test_nodeexporter.py
@@ -10,14 +10,6 @@ from charmhelpers.core import hookenv
 from charmtest import CharmTest
 
 
-class HttpStub:
-
-    port = None
-
-    def configure(self, port=None):
-        self.port = port
-
-
 class Snap:
 
     name = "snap"
@@ -49,14 +41,11 @@ class FooTest(CharmTest):
 
     def setUp(self):
         super().setUp()
-        self.http = HttpStub()
-        # self.charm_dir = os.getcwd()
-        # os.environ["CHARM_DIR"] = self.charm_dir
-        basic.init_config_states()
-        hookenv.config()["snap_proxy"] = ""
-        data_changed("snap.proxy", "")
-        code_dir = os.getcwd()
-        charm_dir = hookenv.charm_dir()
+        self._init_reactive()
+        self._init_snap_layer()
+        self._init_fake_juju()
+
+    def _init_fake_juju(self):
         tools_dir = "/var/lib/juju/tools/machine-0"
         os.makedirs(tools_dir)
         jujud_path = os.path.join(tools_dir, "jujud")
@@ -64,14 +53,23 @@ class FooTest(CharmTest):
             jujud.write("#!/bin/sh\necho 2.0.3\n")
         os.chmod(jujud_path, 0o755)
 
+        self.resource_get = ResourceGet()
+        self.fakes.processes.add(self.resource_get)
+
+    def _init_reactive(self):
+        basic.init_config_states()
+        code_dir = os.getcwd()
+        charm_dir = hookenv.charm_dir()
         for sub_path in ["reactive", "layer.yaml"]:
             source = os.path.join(code_dir, sub_path)
             target = os.path.join(charm_dir, sub_path)
             os.symlink(source, target)
+
+    def _init_snap_layer(self):
+        hookenv.config()["snap_proxy"] = ""
+        data_changed("snap.proxy", "")
         self.snap = Snap()
         self.fakes.processes.add(self.snap)
-        self.resource_get = ResourceGet()
-        self.fakes.processes.add(self.resource_get)
 
     def test_install_snap(self):
         os.environ["JUJU_HOOK_NAME"] = "install"
