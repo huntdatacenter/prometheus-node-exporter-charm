3f859fd8184e1dfcb12eeb4f458b381efd28346f
Matus Kosut <matus.kosut@ntnu.no>
2020-06-02 11:13:56 +0200

    assure service is enabled when starting node exporter

--- lib/charms/layer/prometheus_node_exporter.py
+++ lib/charms/layer/prometheus_node_exporter.py
@@ -3,6 +3,7 @@ from charmhelpers.core.host import (
     service_running,
     service_start
 )
+from charmhelpers.core.host import service as service_cmd
 
 NODE_EXPORTER_BIN = "/usr/bin/prometheus-node-exporter"
 NODE_EXPORTER_SERVICE = "/etc/systemd/system/prometheus-node-exporter.service"
@@ -13,4 +14,5 @@ def start_restart(service):
     if service_running(service):
         service_restart(service)
     else:
+        service_cmd('enable', service)
         service_start(service)
